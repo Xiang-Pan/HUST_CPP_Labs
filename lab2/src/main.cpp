0e28cdf8cf23ea2ea4a96673c274391a7bb3e6c3
Hover <hover@hust.edu.cn>
2018-10-12 02:11:14 +0800

    finish lab1

---
+++ lab2/src/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <unistd.h>
+#include <string.h>
+#include "my_debug.h"
+#include "lab1.h"
+// #define DEBUG
+// // #undef DEBUG
+
+// #ifdef DEBUG
+// #endif
+
+using namespace std;
+
+
+//Helper Function
+//Judge Funciton 
+
+
+int main(int argc, char *argv[]) 
+{
+    if(strcmp(argv[1], "stack") == 0)
+    {
+        debug("stack!");
+        stack_main(argc-1,argv+1);
+    }
+    else if(strcmp(argv[1], "stack") == 0)
+    {
+        debug("stack!");
+    }
+    return 0;
+}
+
